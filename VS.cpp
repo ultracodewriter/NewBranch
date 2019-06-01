e40fc14a69fd2f8bb112004d29b1336d95239ce6
codemaster <2917853072@qq.com>
2019-06-01 11:12:28 +0800

    MUITIPLY TABLE

--- VS.cpp
+++ VS.cpp
@@ -1,6 +1,14 @@
 include<iostream>
+
 int main()
 {
 	std::cout << "Hello World!!!" << std::endl;
+
+for(int i = 1; i < 10; i++)
+	for(int j = i + 1; j < 10; j++)
+		std::cout << i * j << " = " << i << " + " << j <<"  ";
+	std::cout << std::endl;
+
 	std::cin.get();
 }
+
