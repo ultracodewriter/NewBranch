7f5ea35371e080babce5bb79713140cab6053bb2
ultracodewriter <51253087+ultracodewriter@users.noreply.github.com>
2019-06-15 08:30:38 +0800

    Add files via upload

--- VS.cpp
+++ VS.cpp
@@ -2,7 +2,7 @@ include<iostream>
 
 int main()
 {
-	std::cout << "Hello World!!!" << std::endl;
+	std::cout << "Good Morning!!!" << std::endl;
 
 for(int i = 1; i < 10; i++)
 	for(int j = i + 1; j < 10; j++)
@@ -12,3 +12,46 @@ for(int i = 1; i < 10; i++)
 	std::cin.get();
 }
 
+class Entity 
+{
+private:
+	int m_Speed;
+	std::string rank;
+
+public:
+	Entity()
+	{
+		m_Speed = 5;
+		rank = "Beginner";
+	}
+}
+
+
+class Entity1
+{
+private:
+	int m_A ;
+public:
+	Entity1()
+	{
+		m_A = 5;
+		std::cout << m_A << std::endl;	
+	};
+
+	~Entity1
+	{
+		std::cout << "Enttity is destructed !" << std::endl;
+	};
+	
+};
+
+/*
+#include<iostream>
+
+int main()
+{
+	std::cout << "Happy Dragon Boat Festival" << std::endl;
+
+	std::cin.get();
+}*/
+//Happy Dragon Boat Festival to HEHEHE!!!!!
